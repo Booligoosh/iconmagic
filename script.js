14d4d1055986344f0a930012df2d5c71255b4361
Booligoosh <booligoosh@optusnet.com.au>
2016-02-11 07:45:34 +1100

    converted script into function iconAdd()

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
+var iconAdd = function() {
 //Data grabber:
 var iconUrl = document.forms["form"]["icon"].value;
 //Head adder script:
 document.getElementsByTagName('head')[0].appendChild(iconUrl);
+}
