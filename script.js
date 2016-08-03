44d424332b90c343c290778a49030b2dcb62d335
Booligoosh <booligoosh@optusnet.com.au>
2016-08-03 16:07:57 +1000

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-var iconAdd = function() {
-//Data grabber:
-var iconUrl = document.forms["form"]["icon"].value;
-//Head adder script:
-document.getElementsByTagName('head')[0].appendChild(iconUrl);
-}
